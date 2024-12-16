78b476367da5f397644a98e768af23e64fa8c9a1
Gabriel <gabriel.silvam@outlook.com.br>
2024-12-16 19:13:28 -0300

    first cot

--- js/script.js
+++ js/script.js
@@ -17,32 +17,3 @@ document.querySelectorAll('nav ul li a').forEach(anchor => {
     });
 });
 
-async function enviarFormulario() {
-    const nome = document.getElementById("nome").value;
-    const email = document.getElementById("email").value;
-    const telefone = document.getElementById("telefone").value;
-    const mensagem = document.getElementById("mensagem").value;
-
-    try {
-        // Substitua pela URL do seu backend no Render
-        const response = await fetch('hhttps://empresa-jdj6.onrender.com/', {  // URL pública do seu backend no Render
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ nome, email, telefone, mensagem })
-        });
-
-        const data = await response.json();
-        alert(data.message);
-
-        // Limpa os campos do formulário
-        document.getElementById("nome").value = '';
-        document.getElementById("email").value = '';
-        document.getElementById("telefone").value = '';
-        document.getElementById("mensagem").value = '';
-    } catch (error) {
-        console.error("Erro ao enviar formulário:", error);
-        alert("Erro ao enviar o formulário.");
-    }
-}
